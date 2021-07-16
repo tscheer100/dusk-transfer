b076fc009568aeaef3a0b8a8e1e417b8307f8cbf
tscheer100 <thomas.k.scheer@gmail.com>
2021-07-16 16:02:46 -0500

    added open_bank command for ease

--- cogs/bank.py
+++ cogs/bank.py
@@ -1,17 +1,20 @@
+from discord import member
 from discord.ext.commands.core import command
 import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-from discord.ext.commands import errors
+from discord.ext.commands import context, errors
 import pymongo
 from pymongo import MongoClient
 from pymongo import errors
+from motor import motor_asyncio
+from typing import Union
 
 load_dotenv('./.env')
 MONGO_URL = os.getenv('MONGO_URL')
 
-cluster = MongoClient(MONGO_URL)
+cluster = motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 db = cluster['dusk-bank']
 collection = db['bank']
 
@@ -21,21 +24,52 @@ collection = db['bank']
 class Bank(commands.Cog):
     def __init__(self, client):
         self.client = client
-
+    
     # Events
     @commands.Cog.listener()
+    async def on_ready(self):
+        print("bank ready")
+
+    @commands.Cog.listener()
     async def on_member_join(self, member):
-        print("A member has joined")
-        id = member.id
-        wallet = 0
-        bank = 0
-        new = {'_id': id, 'wallet': wallet, 'bank': bank}
-        collection.insert_one(new)
-        print(new)
-    @on_member_join.error()
-    async def on_member_join_error(self, err):
-        if isinstance(err, pymongo.errors):
-            print("User already has a balance")
+        await self.open_bank(member)
+
+    async def open_bank(self, ctx: Union[commands.Context, discord.Member]):
+        ID = None
+        name = None
+        if isinstance(ctx, commands.Context):
+            ID = ctx.author.id
+            name = ctx.author.name
+        elif isinstance(ctx, discord.Member):
+            ID = ctx.id
+            name = ctx.name
+        result = await collection.find_one({'_id': ID})
+        if result == None:
+            print(result)
+            wallet = 0
+            bank = 0
+            new = {'_id': ID, 'name': name, 'wallet': wallet, 'bank': bank}
+            await collection.insert_one(new)
+            print(new)
+        else:
+            print("member already has a bank")
+
+
+
+    
+#FIX THE FOLLOWING!!
+
+    # @on_member_join.error()
+    # async def on_member_join_error(self, err):
+    #     if isinstance(err, pymongo.errors.DuplicateKeyError):
+    #         print("User already has a balance")
+
+    # commands
+    # @commands.command(aliases = ["bal", "wallet", "money"])
+    # async def balance( self, ctx):
+    #     ID = ctx.author.id
+    #     print(ID)
+        
 
     # @commands.command()
     # @commands.command(1,60, commands.BucketType.user)
